2a757536c9564f805d4dc08de1919c3bff317553
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-20 00:16:58 +0530

    finish

--- 3. spring-config-with-java/src/main/java/com/nasyxnadeem/JavaConfigSpringApplication.java
+++ 3. spring-config-with-java/src/main/java/com/nasyxnadeem/JavaConfigSpringApplication.java
@@ -2,6 +2,7 @@ package com.nasyxnadeem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class JavaConfigSpringApplication {
@@ -9,5 +10,7 @@ public class JavaConfigSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaConfigSpringApplication.class, args);
 	}
+
+	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 
 }
