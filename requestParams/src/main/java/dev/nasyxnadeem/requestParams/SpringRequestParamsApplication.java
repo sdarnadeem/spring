9fe819dcde2cd178eeb537b1d33bbb261b113143
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-26 10:53:13 +0530

    first commit

---
+++ requestParams/src/main/java/dev/nasyxnadeem/requestParams/SpringRequestParamsApplication.java
@@ -0,0 +1,13 @@
+package dev.nasyxnadeem.requestParams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRequestParamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRequestParamsApplication.class, args);
+	}
+
+}
---
+++ requestParams/src/test/java/dev/nasyxnadeem/requestParams/SpringRequestParamsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nasyxnadeem.requestParams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRequestParamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
