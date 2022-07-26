c365b2bc30d046a6955f6ed8bcd2a63521661c58
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-26 10:44:51 +0530

    remove 3

--- resthibernate/src/main/java/dev/nasyxnadeem/hibernate/SpringBootRestWithHibernateApplication.java
+++ resthibernate/src/main/java/dev/nasyxnadeem/hibernate/SpringBootRestWithHibernateApplication.java
@@ -13,6 +13,7 @@ public class SpringBootRestWithHibernateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestWithHibernateApplication.class, args);
+		// hdg
 	}
 
 }
